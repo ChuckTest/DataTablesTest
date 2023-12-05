31f7a8b13c7a5f898dcd3ecc775fa525d78ad38e
Chuck LU <chucklu2015@outlook.com>
2023-12-05 23:58:46 +0800

    test with datatables with bootstrap

--- test.js
+++ test.js
@@ -224,7 +224,7 @@ function formatDate(data) {
                               }
     },
     {
-      data:null,
+      //data:null,
     render: function(data, type, row) {
                                   return '<a class="btn btn-primary"  href="/UK_60_Dev_Admin/Nomination/GetNominationDetail?NominationID=' +
                                       row.TransactionID +
